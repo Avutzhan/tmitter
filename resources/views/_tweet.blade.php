a239cce67ea48086322645b98892333a1299f368
Avutzhan Dautov <dautov92@list.ru>
2020-07-21 16:10:31 +0600

    Design the Timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,13 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci aliquid architecto doloremque eaque eius error harum illo ipsam natus nisi provident quo quod, soluta, totam? Omnis qui sint tempore.
+        </p>
+    </div>
+</div>
